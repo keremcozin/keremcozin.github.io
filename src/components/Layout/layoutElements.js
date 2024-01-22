4b74f2e078fa7232b18cfbacf976dc947c8dd596
Kerem Caner Ozin <ozin.keremcaner@gmail.com>
2024-01-22 22:41:56 +0300

    Lots of updates

--- src/components/Layout/layoutElements.js
+++ src/components/Layout/layoutElements.js
@@ -3,7 +3,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 1000px;
+  max-width: 1000px; /* Adjust the maximum width as needed */
   margin: 0 auto;
-  padding: 0 20px;
+  padding: 0 20px; /* Add padding on both sides */
 `;
