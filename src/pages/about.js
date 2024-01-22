8366eb2b2ad11bc8a0f3d6fb04786b80f961f161
Kerem Caner Ozin <ozin.keremcaner@gmail.com>
2024-01-22 22:38:07 +0300

    Lots of updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,27 +3,15 @@
 import React from 'react';
 import { Container as LayoutContainer } from '../components/Layout/layoutElements';
 
-<<<<<<< HEAD
 const aboutHeader = "I'm a relatively new Software Engineer B.S. graduate from National Technical University, Kharkiv Polytechnic Institute. Practiced in object-oriented programming principles and full-stack development with a passion for modern programming techniques.";
 const aboutContentOne = "I recently discovered this web site for making fancy resumes, check it out if you wanna try something new;";
 const aboutContentTwo = "And some more content here...";
 
-<<<<<<< HEAD
 const About = () => {
   return (
     <LayoutContainer>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'left',
-          margin: '0 auto',
-          padding: '0 20px',
-          maxWidth: '600px'
-        }}
-      >
-        <h3>{aboutHeader}</h3>
+      <div style={{ textAlign: 'left', marginTop: '20px' }}>
+        <h2>{aboutHeader}</h2>
         <p>{aboutContentOne}</p>
         <p>
           <a href="https://www.standardresume.co" target="_blank" rel="noopener noreferrer">
@@ -34,28 +22,6 @@ const About = () => {
       </div>
     </LayoutContainer>
   );
-=======
-=======
->>>>>>> 25d454bba3c804e85c5c0ce0696f7fbbc3f22280
-const aboutHeader = "This section is about introductions.";
-const aboutContent = "And so on...";
-const About = () => {
-    return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'flex-start',
-                alignItems: 'flex-start',
-                height: '100vh'
-            }}
-        >
-            <h1> {aboutHeader} </h1>
-            <br />
-            <p> {aboutContent} </p>
-        </div>
-    );
->>>>>>> 25d454bba3c804e85c5c0ce0696f7fbbc3f22280
 };
 
 export default About;
